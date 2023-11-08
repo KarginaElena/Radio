0a1d335c325e1f8c6d9a969ba7a66def39c3ac56
Elena <len.kargina2010@yandex.ru>
2023-11-08 21:12:25 +0400

    Updated

--- src/main/java/org/example/Radio.java
+++ src/main/java/org/example/Radio.java
@@ -4,13 +4,22 @@ public class Radio {
 
     private int currentStation;
     private int currentVolume;
+    private int maxStation;
+
+    public Radio() {
+        this.maxStation = 9;
+    }
+
+    public Radio(int countStations) {
+        this.maxStation = countStations - 1;
+    }
 
     public int getCurrentStation() {
         return currentStation;
     }
 
     public void setCurrentStation(int currentStation) {
-        if (currentStation > 9) {
+        if (currentStation > maxStation) {
             return;
         }
         if (currentStation < 0) {
@@ -34,7 +43,7 @@ public class Radio {
     }
 
     public void nextStation() {
-        if (currentStation < 9) {
+        if (currentStation < maxStation) {
             currentStation++;
         } else {
             currentStation = 0;
@@ -45,7 +54,7 @@ public class Radio {
         if (currentStation > 0) {
             currentStation--;
         } else {
-            currentStation = 9;
+            currentStation = maxStation;
         }
     }
 
